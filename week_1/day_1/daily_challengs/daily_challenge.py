13f54ce4a305c3c64f3a2718538c4e951553d9c7
imadorx <ikaaouane@gmail.com>
2025-08-12 10:59:40 +0100

    first commit

---
+++ week_1/day_1/daily_challengs/daily_challenge.py
@@ -0,0 +1,21 @@
+# ------ Challenge 1
+
+# number = int (input("give your number : "))
+# lenght = int (input("give your lenght : "))
+ 
+# my_list =[]
+# for i in range (1, lenght +1) : 
+#     my_list.append(number*i)
+# print(my_list)
+
+# ------ Challenge 2
+
+text = input('entre your text : ')
+
+result = "" 
+if char in text :
+    if  not result or char != result[-1] :
+        result += char 
+print(result)
+
+
