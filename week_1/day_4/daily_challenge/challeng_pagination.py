2b74e2455673b43805d0657f1854bbe8a7346b12
imadorx <ikaaouane@gmail.com>
2025-08-31 02:20:12 +0100

    update_day4

--- week_1/day_4/daily_challenge/challeng_pagination.py
+++ week_1/day_4/daily_challenge/challeng_pagination.py
@@ -1,28 +1,26 @@
 import math
 
-class Pagination :
+class Pagination:
 
-    def __init__(self , items = None, page_size = 10):
-       
-       if items == None  :
-         items=[]
-         self.items = items
-         self.page_size = page_size
-         self.current_idx = 0    
-         self.total_page = math.ceil(len(self.items)/self.page_size)
+    def __init__(self, items=None, page_size=10):
+        if items is None:
+            items = []
+        self.items = items
+        self.page_size = page_size
+        self.current_idx = 0
+        self.total_pages = math.ceil(len(self.items) / self.page_size)
 
-    def get_visible_items( self):
+    def get_visible_items(self):
         start = self.current_idx * self.page_size
         end = start + self.page_size
         return self.items[start:end]
-    
+
     def go_to_page(self, page_num):
-        def go_to_page(self, page_num):
-         if page_num < 1 or page_num > self.total_pages:
+        if page_num < 1 or page_num > self.total_pages:
             raise ValueError("Page number out of range")
         self.current_idx = page_num - 1
         return self
-    
+
     def first_page(self):
         self.current_idx = 0
         return self
@@ -30,7 +28,7 @@ class Pagination :
     def last_page(self):
         self.current_idx = self.total_pages - 1
         return self
-    
+
     def next_page(self):
         if self.current_idx < self.total_pages - 1:
             self.current_idx += 1
@@ -40,7 +38,7 @@ class Pagination :
         if self.current_idx > 0:
             self.current_idx -= 1
         return self
-    
+
     def __str__(self):
         return "\n".join(self.get_visible_items())
 
