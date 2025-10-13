ec8cf39226a241edfe4e4612000a0152b92d9df0
imadorx <ikaaouane@gmail.com>
2025-10-13 14:41:02 +0100

    update

--- week_3/day_2/exercises/exercises_xp_ninja.js
+++ week_3/day_2/exercises/exercises_xp_ninja.js
@@ -12,7 +12,32 @@ let personeOne ={
   FullName: "sami hilal",
   Mass: 80 ,
   Height : 175
-//   Bmi: function() {
-//     return this.Mass / (this.Height * this.Height);
-//   }
+  Bmi: function() {
+    return this.Mass / (this.Height * this.Height);
+  }
 };
+
+// exercice 2
+
+function calculateAverage(gradesList) {
+  let sum = 0;
+  for (let i = 0; i < gradesList.length; i++) {
+    sum += gradesList[i];
+  }
+  return sum / gradesList.length;
+}
+
+function findAvg(gradesList) {
+  let average = calculateAverage(gradesList);
+  console.log("Average:", average);
+
+  if (average > 65) {
+    console.log("You passed!");
+  } else {
+    console.log("You failed and must repeat the course");
+  }
+}
+
+
+findAvg([80, 90, 70]); 
+findAvg([50, 60, 55]); 
